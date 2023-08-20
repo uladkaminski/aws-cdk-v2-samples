cb3cefc803ff7162d1f4fc798daf9c2cab5257ae
uladkaminski <kaminski.uladzislau@gmail.com>
2023-08-20 20:47:21 +0100

    s3_bucket sample

---
+++ s3_bucket/test/s3_bucket_creation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3BucketCreation from '../lib/s3_bucket_creation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3_bucket_creation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3BucketCreation.S3BucketCreationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
