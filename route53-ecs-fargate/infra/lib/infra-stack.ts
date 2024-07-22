1402ae7c6f5f56cfc29859651abb150920ec3298
uladkaminski <kaminski.uladzislau@gmail.com>
2024-07-22 12:08:31 +0100

    formatting

--- route53-ecs-fargate/infra/lib/infra-stack.ts
+++ route53-ecs-fargate/infra/lib/infra-stack.ts
@@ -7,7 +7,6 @@ import * as ecr from 'aws-cdk-lib/aws-ecr';
 import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-import * as fs from 'fs';
 import { Construct } from 'constructs';
 
 export class InfraStack extends cdk.Stack {
@@ -18,44 +17,43 @@ export class InfraStack extends cdk.Stack {
     const cluster = new ecs.Cluster(this, 'EcsCluster', { vpc });
     const ecrRepository = ecr.Repository.fromRepositoryName(this, 'EcrRepo', 'your-repo-name');
     const taskDef = new ecs.FargateTaskDefinition(this, 'TaskDef');
-    const envVariables = JSON.parse(fs.readFileSync('env.json', 'utf-8'));
 
 
     const container = taskDef.addContainer('AppContainer', {
-        image: ecs.ContainerImage.fromEcrRepository(ecrRepository),
-        memoryLimitMiB: 512,
-        cpu: 256,
-        logging: new ecs.AwsLogDriver({ streamPrefix: 'AppContainer' }),
-        environment: {
-        },
+      image: ecs.ContainerImage.fromEcrRepository(ecrRepository),
+      memoryLimitMiB: 512,
+      cpu: 256,
+      logging: new ecs.AwsLogDriver({ streamPrefix: 'AppContainer' }),
+      environment: {
+      },
 
     });
 
-     // ACM Certificate
-     const zone = route53.HostedZone.fromLookup(this, 'Zone', { domainName: 'example.com' });
-     const cert = new acm.Certificate(this, 'Certificate', {
+    // ACM Certificate
+    const zone = route53.HostedZone.fromLookup(this, 'Zone', { domainName: 'example.com' });
+    const cert = new acm.Certificate(this, 'Certificate', {
       domainName: 'test.example.com',
       subjectAlternativeNames: ['cool.example.com', 'test.example.net'],
       validation: acm.CertificateValidation.fromDnsMultiZone({
         'test.example.com': zone,
       }),
     });
-     
- 
+
+
     container.addPortMappings({ containerPort: 80 });
     const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
-        cluster,
-        taskDefinition: taskDef,
-        publicLoadBalancer: true, // if you want the LB to be internet-facing,
-        desiredCount: 2, // amount of instances
-        protocol: elbv2.ApplicationProtocol.HTTPS,
-        certificate: cert,
+      cluster,
+      taskDefinition: taskDef,
+      publicLoadBalancer: true, // if you want the LB to be internet-facing,
+      desiredCount: 2, // amount of instances
+      protocol: elbv2.ApplicationProtocol.HTTPS,
+      certificate: cert,
     });
 
     new route53.ARecord(this, 'SubdomainRecord', {
-        zone: zone,
-        recordName: 'test.example.com',
-        target: route53.RecordTarget.fromAlias(new route53Targets.LoadBalancerTarget(fargateService.loadBalancer))
+      zone: zone,
+      recordName: 'test.example.com',
+      target: route53.RecordTarget.fromAlias(new route53Targets.LoadBalancerTarget(fargateService.loadBalancer))
     });
 
   }
